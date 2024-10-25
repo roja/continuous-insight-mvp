bdf426cce267b520da2534ac9788cd07fdda335f
roja <roja@arbia.co.uk>
2024-10-25 16:56:18 +0100

    sort out migration

--- alembic/versions/93ea8ee57d01_add_deleted_at_columns.py
+++ alembic/versions/93ea8ee57d01_add_deleted_at_columns.py
@@ -13,14 +13,30 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '93ea8ee57d01'
-down_revision: Union[str, None] = 'add_deleted_at_columns'
+down_revision: Union[str, None] = 'be2af536c0ca'
 branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None                                                                                                                                                                                                                                                      
+                                                                                                                                                                                                                                                                            
+                                                                                                                                                                                                                                                                             
+def upgrade() -> None:                                                                                                                                                                                                                                                      
+     # Create new tables with the deleted_at column                                                                                                                                                                                                                          
+     with op.batch_alter_table('users') as batch_op:                                                                                                                                                                                                                         
+         batch_op.add_column(sa.Column('deleted_at', sa.DateTime(timezone=True), nullable=True))                                                                                                                                                                             
+                                                                                                                                                                                                                                                                             
+     with op.batch_alter_table('audits') as batch_op:                                                                                                                                                                                                                        
+         batch_op.add_column(sa.Column('deleted_at', sa.DateTime(timezone=True), nullable=True))    
 
+     with op.batch_alter_table('companies') as batch_op:                                                                                                                                                                                                                         
+         batch_op.add_column(sa.Column('deleted_at', sa.DateTime(timezone=True), nullable=True))                                                                                                                                                                           
+                                                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                                                             
+def downgrade() -> None:                                                                                                                                                                                                                                                    
+     # Remove deleted_at columns                                                                                                                                                                                                                                             
+     with op.batch_alter_table('users') as batch_op:                                                                                                                                                                                                                         
+         batch_op.drop_column('deleted_at')                                                                                                                                                                                                                                  
+                                                                                                                                                                                                                                                                             
+     with op.batch_alter_table('audits') as batch_op:                                                                                                                                                                                                                        
+         batch_op.drop_column('deleted_at') 
 
-def upgrade() -> None:
-    pass
-
-
-def downgrade() -> None:
-    pass
+     with op.batch_alter_table('companies') as batch_op:                                                                                                                                                                                                                        
+         batch_op.drop_column('deleted_at')
